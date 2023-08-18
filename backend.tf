2451593636f172807cf786fda805551b4da29a3c
himanshu <himanshu@gmail.com>
2023-08-18 18:30:07 +0530

    updated the code

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "s3-datadog"
-    key    = "datadog/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "datadog_table"
-    encrypt = true
-  }
-}
