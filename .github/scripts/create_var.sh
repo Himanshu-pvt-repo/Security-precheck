02edf3fad339ffa366ae6a84627804ce9dd1a672
Himanshu <32194294+Himanshu-pvt-repo@users.noreply.github.com>
2023-08-21 23:54:39 +0530

    Update create_var.sh

--- .github/scripts/create_var.sh
+++ .github/scripts/create_var.sh
@@ -10,7 +10,7 @@ datadog_app_key=$5
 cat << EOF > terraform.tfvars
 datadog_api_key_name = "$datadog_api_key_name"
 enable_datadog_integration = $enable_datadog_integration
-datadog_host_tags=[$datadog_host_tags]
+datadog_host_tags=$datadog_host_tags
 datadog_api_key= "$datadog_api_key"
 datadog_app_key= "$datadog_app_key"
 EOF
