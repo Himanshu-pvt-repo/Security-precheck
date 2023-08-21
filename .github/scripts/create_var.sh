b2fd66940d4f2844ae2df5bc32fe4d3163337cb2
Himanshu <32194294+Himanshu-pvt-repo@users.noreply.github.com>
2023-08-21 18:11:13 +0530

    Create create_var.sh

---
+++ .github/scripts/create_var.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+datadog_api_key_name=$1
+enable_datadog_integration=$2
+datadog_host_tags=$3
+
+# Generate Terraform configuration
+cat << EOF > backend.tf
+datadog_api_key_name = $datadog_api_key_name
+enable_datadog_integration = $enable_datadog_integration
+datadog_host_tags=$datadog_host_tags
+EOF
+
+echo "Terraform Values configuration generated in terraform.tfvars"
