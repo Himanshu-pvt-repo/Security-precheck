a42f8a8c4ffbeeb134ade43a9e1147d0799dfccf
himanshu <himanshu@gmail.com>
2023-09-19 17:30:45 +0530

    updated the datadog bulk

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "3.26.0"
+    }
+  }
+}
+
+# Configure the Datadog provider
+provider "datadog" {
+  app_key = var.datadog_app_key
+  api_key = var.datadog_api_key
+  alias   = "datadog-provider"
+}
+
+
+
+module "bulk_monitor" {
+
+  providers = {
+    datadog = datadog.datadog-provider
+  }
+  source               = "../modules/bulk_monitor"
+  filepath             = "monitor.csv"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "datadog_app_key" {
+  description = "datadog_app_key"
+  type        = string
+  default     = "5d195aaa34c7b6bd29922aa855b9b56949991329"
+}
+
+variable "datadog_api_key" {
+  description = "datadog_api_key"
+  type        = string
+  default     = "38732581af1be9d144bffe0ca8c0767f"
+}
